e449ad833b0960f90bf816442d1a009cbffe7341
UP2040500 <91894849+UP2040500@users.noreply.github.com>
2022-07-27 02:32:23 +0100

    added price ticker to second navbar, added progress scroll container

---
+++ Desktop/website/js/image.js
@@ -0,0 +1,6 @@
+let image = document.getElementById("image");
+let images = ["images/img1.jpeg", "images/img2.jpeg", "images/img3.jpeg", "images/img4.jpeg"];
+setInterval(function () {
+  let random = Math.floor(Math.random() * 4);
+  image.src = images[random];
+}, 1000);
