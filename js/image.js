8a723747b6333b06d3e8173a791543093b10c896
UP2040500 <91894849+UP2040500@users.noreply.github.com>
2022-08-02 00:10:40 +0100

    Fixed mobile formatting, re organised pages

--- js/image.js
+++ js/image.js
@@ -3,5 +3,4 @@ let images = ["images/img1.jpeg", "images/img2.jpeg", "images/img3.jpeg", "image
 setInterval(function () {
   let random = Math.floor(Math.random() * 4);
   image.src = images[random];
-  console.log(image.src);
 }, 1000);
