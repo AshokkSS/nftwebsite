5fea17c3e0b81b80ca60a37d4c9b6c83122b7796
UP2040500 <91894849+UP2040500@users.noreply.github.com>
2022-08-02 16:00:28 +0100

    Light mode toggle complete just needs to save if in light mode or not

--- js/light-mode.js
+++ js/light-mode.js
@@ -1,17 +1,12 @@
 function myFunction2() {
     var body = document.body;
-    var textinfo = document.querySelector(".textinfo");
     var navbar = document.querySelector(".navbar");
     var mininavbar = document.querySelector(".mininavbar");
-    var ep = document.querySelector(".episode__number");
-    var ep2 = document.querySelector(".episode__content");
     var logo = document.querySelector(".logo");
     var navlinks = document.querySelector(".nav-links");
+    var h2 = document.querySelector("h2")
     navbar.classList.toggle("dark-mode-navbar");
     mininavbar.classList.toggle("dark-mode-mininavbar");
-    body.classList.toggle("dark-mode");
-    textinfo.classList.toggle("dark-mode");
-    ep.classList.toggle("dark-mode");
-    ep2.classList.toggle("dark-mode");
+    body.classList.toggle("dark-mode"); 
     logo.classList.toggle("logo-light");
     navlinks.classList.toggle("nav-links-light");}
