0fedfaa26515c0067af255d79e1756a4f05d9fca
UP2040500 <91894849+UP2040500@users.noreply.github.com>
2022-08-03 16:04:51 +0100

    Styling done, just need to add guides then complete

--- js/light-mode.js
+++ js/light-mode.js
@@ -1,12 +1,28 @@
-function myFunction2() {
-    var body = document.body;
-    var navbar = document.querySelector(".navbar");
-    var mininavbar = document.querySelector(".mininavbar");
-    var logo = document.querySelector(".logo");
-    var navlinks = document.querySelector(".nav-links");
-    var h2 = document.querySelector("h2")
-    navbar.classList.toggle("dark-mode-navbar");
-    mininavbar.classList.toggle("dark-mode-mininavbar");
-    body.classList.toggle("dark-mode"); 
-    logo.classList.toggle("logo-light");
-    navlinks.classList.toggle("nav-links-light");}
+const toggleSwitch = document.querySelector(
+    '.switch input[type="checkbox"]'
+);
+console.log(localStorage.getItem('lightMode'));
+if(localStorage.getItem('lightMode') == 'enabled'){
+    theme.setAttribute('href', 'light.css');
+    toggleSwitch.checked = true;
+}
+
+function toggleTheme() {
+    var theme = document.getElementById('theme');
+    if (theme.getAttribute('href') == 'styles.css') {
+
+        theme.setAttribute('href', 'light.css');
+        localStorage.setItem('lightMode', 'enabled');
+        console.log(localStorage.getItem('lightMode'));
+
+    }
+    else {
+
+        theme.setAttribute('href', 'styles.css');
+        localStorage.setItem('lightMode', 'disabled');
+        console.log(localStorage.getItem('lightMode'));
+
+    }
+
+}
+;
