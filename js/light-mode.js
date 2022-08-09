3054bc14f9ba7515c4b08047c8588253f17227e7
UP2040500 <91894849+UP2040500@users.noreply.github.com>
2022-08-10 00:06:58 +0100

    almost complete, pictures need to be updated then complete

--- js/light-mode.js
+++ js/light-mode.js
@@ -1,28 +1,19 @@
-const toggleSwitch = document.querySelector(
-    '.switch input[type="checkbox"]'
-);
-console.log(localStorage.getItem('lightMode'));
-if(localStorage.getItem('lightMode') == 'enabled'){
-    theme.setAttribute('href', 'light.css');
-    toggleSwitch.checked = true;
+var toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+console.log(localStorage.getItem("lightMode"));
+if (localStorage.getItem("lightMode") == "enabled") {
+  theme.setAttribute("href", "light.css");
+  toggleSwitch.checked = true;
 }
 
 function toggleTheme() {
-    var theme = document.getElementById('theme');
-    if (theme.getAttribute('href') == 'styles.css') {
-
-        theme.setAttribute('href', 'light.css');
-        localStorage.setItem('lightMode', 'enabled');
-        console.log(localStorage.getItem('lightMode'));
-
-    }
-    else {
-
-        theme.setAttribute('href', 'styles.css');
-        localStorage.setItem('lightMode', 'disabled');
-        console.log(localStorage.getItem('lightMode'));
-
-    }
-
+  var theme = document.getElementById("theme");
+  if (theme.getAttribute("href") == "styles.css") {
+    theme.setAttribute("href", "light.css");
+    localStorage.setItem("lightMode", "enabled");
+    console.log(localStorage.getItem("lightMode"));
+  } else {
+    theme.setAttribute("href", "styles.css");
+    localStorage.setItem("lightMode", "disabled");
+    console.log(localStorage.getItem("lightMode"));
+  }
 }
-;
