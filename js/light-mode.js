bdd281ac19cc901146cf18c368cd81dce9ce1bb6
UP2040500 <91894849+UP2040500@users.noreply.github.com>
2022-08-11 00:05:56 +0100

    website completed

--- js/light-mode.js
+++ js/light-mode.js
@@ -1,18 +1,18 @@
 var toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 console.log(localStorage.getItem("lightMode"));
 if (localStorage.getItem("lightMode") == "enabled") {
-  theme.setAttribute("href", "light.css");
+  theme.setAttribute("href", "styles/light.css");
   toggleSwitch.checked = true;
 }
 
 function toggleTheme() {
   var theme = document.getElementById("theme");
-  if (theme.getAttribute("href") == "styles.css") {
-    theme.setAttribute("href", "light.css");
+  if (theme.getAttribute("href") == "styles/styles.css") {
+    theme.setAttribute("href", "styles/light.css");
     localStorage.setItem("lightMode", "enabled");
     console.log(localStorage.getItem("lightMode"));
   } else {
-    theme.setAttribute("href", "styles.css");
+    theme.setAttribute("href", "styles/styles.css");
     localStorage.setItem("lightMode", "disabled");
     console.log(localStorage.getItem("lightMode"));
   }
